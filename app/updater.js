ca77accc93e66cf8db33e84df30f9e067ce0462a
David Moodie <davidmoodie12@gmail.com>
2016-06-16 23:58:27 +0100

    Allow user to choose whether or not to update to newer version of editor if found

--- app/updater.js
+++ app/updater.js
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 // import semver from 'semver'
-const semver = require('semver')
+import semver from 'semver'
 import manifest from './package.json'
 
 import {remote, shell} from 'electron'
@@ -24,16 +24,22 @@ const packageURL = 'https://raw.githubusercontent.com/processing/p5.js-editor/ma
 const downloadURL = 'https://github.com/processing/p5.js-editor/releases/download/'
 
 module.exports.check = function () {
-  console.log('Manifest:')
-  console.log(manifest)
+  // console.log('Manifest:')
+  // console.log(manifest)
   if (nodeGlobal.checkedUpdate === true) return false
   nodeGlobal.checkedUpdate = true
   $.ajax({url: packageURL, success: update, cache: false, dataType: 'json'})
   function update (data) {
-    console.log(data)
+    // console.log(data)
     if (semver.gt(data.version, manifest.version)) {
-      // var shouldDownload = confirm('A newer version of P5 is available. Do you want to download it?') // Temporarily just open no matter what
-      var shouldDownload = true
+      const choice = remote.dialog.showMessageBox({
+        type: 'question',
+        title: 'Update to newer version of editor?',
+        message: 'A newer version of the P5.js Editor is available. Do you want to download it?',
+        buttons: ['Yes', 'No'],
+        cancelId: -1
+      })
+      const shouldDownload = choice === 0
       if (shouldDownload) {
         shell.openExternal(downloadURL + 'v' + data.version + '/p5-' + osType + '.zip')
       }
